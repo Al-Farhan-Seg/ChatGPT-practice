16f8b5477d70dd1d39abe8bf401261861d901ba4
Al-Farhan-Seg <farhan.segujja@gmail.com>
2025-10-21 09:03:39 +0300

    Phase One

--- bankSystem.py
+++ bankSystem.py
@@ -2,18 +2,28 @@ class BankAccount:
     def __init__(self, owner):
         self.owner = owner
         self.balance = 0
+        self.transactions = []
 
     def deposit(self, amount):
-        self.balance += amount
+        if amount <= 0:
+            return "Deposit amount MUST be positive...."
+        else:
+            self.balance += amount
+            self.transactions.append({"Transaction":"Deposit",
+                                      "Amount":amount,
+                                      "Balance after": self.balance })
 
-        return self.balance
+        return f"You have successfully deposited {amount}. New Balance {self.balance}........"
     
     def withdraw(self, amount):
         if amount > self.balance:
             print("You have insufficient funds.....")
         else:
             self.balance -= amount
-            print(f"You have successfully withdrawn {amount}. Balance {self.balance}........")
+            self.transactions.append({"Transaction":"Withdraw",
+                                      "Amount":amount,
+                                      "Balance after": self.balance })
+            return f"You have successfully withdrawn {amount}. New Balance {self.balance}........"
 
 acc_1 = BankAccount("Al-Farhan")
 acc_1.deposit(25000)
