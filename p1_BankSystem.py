eb4d0943a9f42cce4e5d928a53bfac1436d5ab90
Al-Farhan-Seg <farhan.segujja@gmail.com>
2025-10-22 04:48:32 +0300

    Phase Two: Encapsulation & Controlled Access

--- p1_BankSystem.py
+++ p1_BankSystem.py
@@ -3,8 +3,9 @@ import string, random
 class BankAccount:
     def __init__(self, owner):
         self.owner = owner
-        self.balance = 0
-        self.transactions = []
+        self.__balance = 0
+        self.__transactions = []
+        self.__pin = 1
 
     def trans_ID(self):
         
@@ -13,40 +14,143 @@ class BankAccount:
         
 
     def deposit(self, amount):
-        if amount <= 0:
-            return "\nDeposit amount MUST be positive...."
-        else:
-            self.balance += amount
-            ID = self.trans_ID()
-            self.transactions.append({"Transaction":"Deposit",
-                                      "ID":ID,
-                                      "Amount":amount,
-                                      "Balance after": self.balance, 
-                                      "Time": datetime.now().strftime("%d-%m-%Y, %H:%M:%S")})
+        print("ACCOUNT DEPOSIT Management".center(50, "_"))
+        attempts = 0
 
-        return f"""\nDeposited: {amount}
-New Balance: {self.balance}
+        # to limit number of PIN input attempts to 3
+        while attempts < 3:
+            try:
+                pin = int(input("Depo-Enter account PIN: "))
+                if pin != self.__pin:
+                    print("Access Denied.....")
+                    attempts += 1
+                else:
+                    if amount <= 0:
+                        return "\nDeposit amount MUST be positive...."
+                    else:
+                        self.__balance += amount
+                        ID = self.trans_ID()
+
+                        # Storing the transaction details into the Transaction Log
+                        self.__transactions.append({"Transaction":"Deposit",
+                                                "ID":ID,
+                                                "Amount":amount,
+                                                "Time": datetime.now().strftime("%d-%m-%Y, %H:%M:%S")})
+
+                        return f"""\nDeposited: {amount}
 Transaction ID: {ID}"""
+            
+            # to catch any non-integer inputs by USER
+            except ValueError:
+                print("PIN should only be an INTEGER")
+        
+        return "Too many Failed attempts" 
     
     def withdraw(self, amount):
-        if amount > self.balance:
-            return "\nYou have insufficient funds....."
-        else:
-            self.balance -= amount
-            ID = self.trans_ID()
-            self.transactions.append({"Transaction":"Withdraw",
-                                      "ID":ID,
-                                      "Amount":amount,
-                                      "Balance after": self.balance, 
-                                      "Time": datetime.now().strftime("%d-%m-%Y, %H:%M:%S")})
-            
-            return f"""\nWithdrawn: {amount}
-New Balance: {self.balance}
+        print("ACCOUNT WITHDRAWAL Management".center(50, "_"))
+        attempts = 0
+
+        # to limit number of PIN input attempts to 3
+        while attempts < 3:
+            try:
+                pin = int(input("Wdraw-Enter account PIN:"))
+                if pin != self.__pin:
+                    print("Access Denied.....")
+                    attempts += 1
+                else:
+                    if amount > self.__balance:
+                        return "\nYou have insufficient funds....."
+                    else:
+                        self.__balance -= amount
+                        ID = self.trans_ID()
+
+                        # Storing the transaction details into the Transaction Log
+                        self.__transactions.append({"Transaction":"Withdraw",
+                                                "ID":ID,
+                                                "Amount":amount,
+                                                "Time": datetime.now().strftime("%d-%m-%Y, %H:%M:%S")})
+                        
+                        return f"""\nWithdrawn: {amount}
 Transaction ID: {ID}"""
+            
+            # to catch any non-integer inputs by USER
+            except ValueError:
+                print("PIN should only be an INTEGER")
+
+        return "Too many Failed attempts"
+    
+        
+    def display_balance(self):
+        print("Balance Display".center(50, "_"))
+        attempts = 0
+
+        # to limit number of PIN input attempts to 3
+        while attempts < 3:
+            try:
+                pin = int(input("Enter account PIN: "))
+                if pin != self.__pin:
+                    print("Access Denied......")
+                    attempts += 1
+                else:
+                    return f"Account Balance: {self.__balance}"
+                
+            # to catch any non-integer inputs by USER
+            except ValueError:
+                print("PIN should only be an INTEGER")
+        
+        return "Too Many Failed Attempts"
+    
+    
+    def get_transactions_1(self):
+        print("TRANSACTION HISTORY".center(40, "_"))
+        for i in self.__transactions:
+            print(" ".ljust(15, "_") + " " + " ".rjust(22, "_"))
+            print("| TITLE".ljust(15, " ") + "|" + "VALUE".rjust(20, " ") + " |")
+            print(" ".ljust(15, "-") + " " + " ".rjust(22, "-"))
+            for k,v in i.items():
+                print(f"| {k}".ljust(15, " ") + "|" + f"{v}".rjust(20, " ") + " |")
+            print(" ".ljust(15, "-") + " " + " ".rjust(22, "-"))
+            print()
+
+
+    def change_pin(self):
+        print("PIN CHANGE Management".center(50, "_"))
+        attempts = 0
+        # to limit the number of PIN input attempts to 3
+        while attempts < 3:
+            try:
+                old_pin = int(input("Enter OLD Account PIN: "))
+                if old_pin != self.__pin:
+                    print("PIN doesnot match saved PIN")
+                else:
+                    new_pin = int(input("Enter NEW account PIN: "))
+                    self.__pin = new_pin
+                    return f"PIN successfully changed"
+            except ValueError:
+                print("PIN must only be INTEGER")
+        return "Too Many Failed Attempts"
+
+            
 
 acc_1 = BankAccount("Al-Farhan")
 print(acc_1.deposit(25000))
+print()
+print(acc_1.display_balance())
+print()
 print(acc_1.withdraw(250000))
+print()
 print(acc_1.deposit(300000))
+print()
 print(acc_1.withdraw(20000))
-#datetime.now()
+print()
+
+print(acc_1.change_pin())
+print()
+print(acc_1.deposit(27000))
+print()
+print(acc_1.withdraw(320000))
+print()
+
+print(acc_1.display_balance())
+
+acc_1.get_transactions_1()
--- p1_Student.py
+++ p1_Student.py
@@ -4,18 +4,120 @@ class Student:
         self.name = name
         self.age = age
         self.course = course
+        self.__gpa = 0
+        self.__email = ""
+        self.courses = []  # bi-directional link: list of Course instances
 
     def introduce(self):
         return f"Hello, my name is {self.name}, I am {self.age} years old and I study {self.course}"
+    
+    def set_gpa(self, value):
+        if 0.0 <= value <= 4.0:
+            self.__gpa = value
+
+    def update_gpa(self, value):
+        self.__gpa = value
+
+    def get_gpa(self):
+        return self.__gpa
+    
+    def set_email(self):
+        self.__email = f"{self.name}".lower() + ".iuiu.ac@gmail.com"
+
+    def get_email(self):
+        return self.__email
+    
+    def show_profile(self):
+        return f"""-----------Student Profile-------------
+Name: {self.name}
+Age: {self.age}
+GPA: {self.get_gpa()}"""
+    
+    # AI gen
+    def add_course(self, course_obj):
+        if course_obj in self.courses:
+            return False
+        self.courses.append(course_obj)
+        return True
+    
+    '''# enroll method to add the current student instance (self) to the course.
+    def enroll(self, course_obj):
+        course_obj.add_student(self)
+        print(course_obj.get_course())'''
+
+    # AI gen
+    # enroll method to add the current student instance (self) to the course.
+    def enroll(self, course_obj):
+        added = course_obj.add_student(self)
+        if added:
+            print(course_obj.get_course())
+        else:
+            print(f"{self.name} is already enrolled in {course_obj.get_course()}")
+    
+
+# Modelling a relationship between Student and Course
+class Course:
+
+    def __init__(self):
+        self.__course_name = "Course"
+        self.__code = "Code"
+        self.__students = []
+
+    def add_student(self, student_obj):
+        if student_obj in self.__students:
+            return "Student is already enrolled in course"
+        else:
+            self.__students.append(student_obj)
+            #AI gen
+            student_obj.add_course(self)
+            return True
+
+    def remove_student(self, student_obj):
+        self.__students.remove(student_obj)
+
+    def list_students(self):
+        print("Student's List".center(50,"_"))
+        for i in self.__students:
+            print(f"- {i.name}")
+
+    def set_course(self, course_name, code):
+        self.__course_name = course_name
+        self.__code = code
+
+    def update_course(self, course_name, code):
+        self.__course_name = course_name
+        self.__code = code
+
+    def get_course(self):
+        return f"You have enrolled for {self.__course_name} ({self.__code})"
+
+
 
 st_1 = Student("Farhan", 22, "BSc.CS")
 st_2 = Student("Bugembe", 20, "LLb")
 st_3 = Student("Lwanga", 25, "BBA")
 
-print(st_1.introduce())
-print(st_2.introduce())
-print(st_3.introduce())
+st_1.set_email()
+print(st_1.get_email())
 
+st_3.__gpa = 2.0
+print(st_3.get_gpa())
 
+#print(st_2.__email)
+st_1.set_gpa(2.67)
 
+print(st_1.show_profile())
 
+course1 = Course()
+course1.set_course("Computer Science", "BSc.CS")
+st_1.enroll(course1)
+st_1.enroll(course1)
+
+st_2.enroll(course1)
+st_3.enroll(course1)
+
+print()
+course1.list_students()
+course1.remove_student(st_2)
+print()
+course1.list_students()
